1c1bc95d2cdd4efb978b4cdcf646b639e620f3e1
Sourjyo Deb <sourjyo@protonmail.com>
2024-11-22 17:05:03 +0530

    too slow

--- app/server.go
+++ app/server.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 	//"golang.org/x/text/cases"
 )
 
@@ -17,7 +18,7 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", "0.0.0.0:6378")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6378")
 		os.Exit(1)
@@ -36,6 +37,9 @@ func main() {
 func handleConnection(conn net.Conn) {
 	// Implement the handling of the incoming connection
 	defer conn.Close()
+	cache := make(map[string]string)
+	duration := make(map[string]string)
+
 	for {
 		buf := make([]byte, 128)
 		_, err := conn.Read(buf[:])
@@ -57,10 +61,11 @@ func handleConnection(conn net.Conn) {
 			length, _ := strconv.Atoi(v[0][1:])
 			for i := 0; i < length; i++ {
 				command = append(command, v[i*2+2])
-				fmt.Println(v[i*2+2])
+				//fmt.Println(v[i*2+2])
 			}
 
-			fmt.Println(length, command)
+			//fmt.Println(length, command)
+			fmt.Println(time.Now())
 			switch command[0] {
 			case "PING":
 				conn.Write([]byte("+PONG\r\n"))
@@ -68,6 +73,34 @@ func handleConnection(conn net.Conn) {
 				conn.Write([]byte("+" + command[1] + "\r\n"))
 			case "COMMAND":
 				conn.Write([]byte("+" + command[1] + "\r\n"))
+			case "SET":
+				conn.Write([]byte("+OK\r\n"))
+				cache[command[1]] = command[2]
+				if len(command) > 3 {
+					delay, _ := strconv.Atoi(command[4])
+					duration[command[1]] = strconv.Itoa(int(time.Now().UnixMilli()) + delay)
+				}
+			case "GET":
+				if val, ok := cache[command[1]]; ok {
+					//fmt.Println("$" + string(3) + "\r\n" + val + "\r\n")
+					if val2, ok := duration[command[1]]; ok {
+						dur, _ := strconv.Atoi(val2)
+						if dur > int(time.Now().UnixMilli()) {
+							conn.Write([]byte("$" + strconv.Itoa(len(val)) + "\r\n" + val + "\r\n"))
+						} else {
+							fmt.Println("Key expired")
+							fmt.Println(dur, time.Now().UnixMilli())
+							conn.Write([]byte("$-1\r\n"))
+						}
+						//fmt.Println("$" + string(3) + "\r\n" + val + "\r\n")
+					} else {
+						fmt.Println("No duration found")
+						conn.Write([]byte("$" + strconv.Itoa(len(val)) + "\r\n" + val + "\r\n"))
+					}
+				} else {
+					fmt.Println("Key not found")
+					conn.Write([]byte("$-1\r\n"))
+				}
 			}
 		}
 	}
