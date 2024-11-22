0fc777967c1256ed7750e2a588a35de4b7b4bb86
Sourjyo Deb <sourjyo@protonmail.com>
2024-11-22 17:06:26 +0530

    too slow port

--- app/server.go
+++ app/server.go
@@ -18,7 +18,7 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 
-	l, err := net.Listen("tcp", "0.0.0.0:6378")
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6378")
 		os.Exit(1)
